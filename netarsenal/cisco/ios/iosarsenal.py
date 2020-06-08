fe73334fc0c784e7870c69e48191f2d0e70a1bb1
Daniel Mendoza <denial@live.com>
2020-06-08 12:35:28 -0500

    created nxos and ios classes

---
+++ netarsenal/cisco/ios/iosarsenal.py
@@ -0,0 +1,21 @@
+from nornir.plugins.tasks.networking import netmiko_send_command
+
+# definitions
+
+# class
+
+
+class IOSArsenal(object):
+    def __init__(self):
+        print("Creating IOS Object")
+
+    def show_cdp_neighbors(self, nornir_object=None, use_textfsm=False):
+        command = "show cdp neighbors detail"
+        params = {
+            "command_string": command,
+            "use_textfsm": use_textfsm,
+        }
+
+        if nornir_object:
+            result = nornir_object.run(task=netmiko_send_command, **params)
+        return result
